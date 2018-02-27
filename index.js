f43c40e2debe701afe0c7e79661ceff6ea14b8ef
Martin <madetho@live.de>
2018-02-27 22:11:55 +0100

    webadmin port

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 
 const Telegram = require('telegram-node-bot'),
     tg = new Telegram.Telegram('550466086:AAHjSFVPBlV4kW3Z1gqyWfG0WSF0nSfty90', {
-        worders: 1
+        worders: 1,
+        webAdmin: {
+            port: process.env.PORT || 7777
+        }
     }),
     PingController = require('./controller/pingController');
 
