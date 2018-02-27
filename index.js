e8bf7d3aa48f60222bd77f3f29ce6985e5d6f134
Martin <madetho@live.de>
2018-02-27 21:33:49 +0100

    ping pong

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const Telegram = require('telegram-node-bot'),
+    tg = new Telegram.Telegram('550466086:AAHjSFVPBlV4kW3Z1gqyWfG0WSF0nSfty90', {
+        worders: 1
+    }),
+    PingController = require('./controller/pingController');
+
+tg.router
+    .when(new Telegram.TextCommand('/ping', 'pingCommand'), new PingController);
